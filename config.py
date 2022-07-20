297218f20e52d6bfd5c9bd1b9d8562682bef4a60
Tesfelix <108227968+Tesfelix@users.noreply.github.com>
2022-07-20 19:12:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 TOKEN = os.environ.get("TOKEN", "")
 ADMIN = int(os.environ.get("ADMIN", ""))
-OWNER = os.environ.get("OWNER", "kenkanasw")
-GROUP = os.environ.get("GROUP", "musikkugroup")
-CHANNEL = os.environ.get("CHANNEL", "musikkuchannel")
+OWNER = os.environ.get("OWNER", "usernamekamu")
+GROUP = os.environ.get("GROUP", "grup")
+CHANNEL = os.environ.get("CHANNEL", "channel")
 PROJECT_NAME = os.environ.get("PROJECT_NAME", "PRIVATE CHAT")
